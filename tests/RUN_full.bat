06bb6ecb4250bcab2b90354fe51b18535f0177da
zedranreb <jibernardez31@gmail.com>
2025-11-10 23:06:54 -0300

    Cambios RUN_FULL.txt

--- tests/RUN_full.bat
+++ tests/RUN_full.bat
@@ -1,5 +1,5 @@
 echo "Compiling input.txt"
-COPY /Y "tests\archivosTest\input.txt" "src\main\resources\input\test.txt"
+COPY /Y "tests\archivosTest\test.txt" "src\main\resources\input\test.txt"
 call mvnw.cmd clean install
 java "-jar" "target\lyc-compiler-1.0.0.jar" "target\input\test.txt"
 COPY "target\output\final.asm" "target\asm\final.asm"
